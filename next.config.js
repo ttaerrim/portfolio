7885344a5301b9c2a98a337e9293ee0fc38c417f
ttaerrim <ltr0121@naver.com>
2023-12-30 20:52:44 +0900

    🔧 next.config.js 변경

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { domains: ['images.clerk.dev'] },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
