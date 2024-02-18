d44a12e48f40d1fe812da79fbe63b77da8fa1a3d
ttaerrim <ltr0121@naver.com>
2024-02-18 16:28:49 +0900

    🔨 career 재직 중 처리

--- components/landing/Introduce.tsx
+++ components/landing/Introduce.tsx
@@ -7,9 +7,18 @@ import { format } from '@formkit/tempo';
 export default function Introduce() {
   const WORKS = [{ name: '레드브릭', position: 'FrontEnd Engineer', period: ['2022-04-25', '2023-07-24'] }];
 
+  const printPeriod = (period: string[]) => {
+    const [start, end] = period;
+
+    return `${format(new Date(start), 'YYYY.MM')} ~ ${end ? format(new Date(end), 'YYYY.MM') : 'now'} (${calcPeriod(
+      period
+    )})`;
+  };
+
   const calcPeriod = (period: string[]) => {
-    const [startDate, endDate] = period;
-    const diff = new Date(endDate).getTime() - new Date(startDate).getTime();
+    const [start, end] = period;
+
+    const diff = (end ? new Date(end) : new Date()).getTime() - new Date(start).getTime();
 
     const diffInMs = Math.abs(diff);
 
@@ -22,16 +31,13 @@ export default function Introduce() {
   return (
     <div className='bg-white flex flex-col justify-center items-center px-12 py-12 pb-20 gap-12 max-w-6xl m-auto rounded-bl-[7rem] rounded-br-[7rem]	'>
       <IntroduceBox />
-      <Section title='Work Experience'>
+      <Section title='Career'>
         <div className='flex flex-col gap-6'>
           {WORKS.map((work) => (
             <div key={work.name}>
               <p className='text-3xl font-semibold'>{work.name}</p>
               <p className='text-base font-light'>{work.position}</p>
-              <p className='font-light text-sm'>{`${format(new Date(work.period[0]), 'YYYY.MM')} ~ ${format(
-                new Date(work.period[1]),
-                'YYYY.MM'
-              )} (${calcPeriod(work.period)})`}</p>
+              <p className='font-light text-sm'>{printPeriod(work.period)}</p>
             </div>
           ))}
         </div>
