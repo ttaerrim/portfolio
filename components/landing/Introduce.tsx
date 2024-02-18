944f2697ca503d2a5ed65c35603168edb1db5693
ttaerrim <ltr0121@naver.com>
2024-02-18 16:24:29 +0900

    🔨 career 부분 추가

--- components/landing/Introduce.tsx
+++ components/landing/Introduce.tsx
@@ -2,11 +2,40 @@ import IntroduceBox from './IntroduceBox';
 import Project from './Project';
 import Section from './Section';
 import Skills from './Skills';
+import { format } from '@formkit/tempo';
 
 export default function Introduce() {
+  const WORKS = [{ name: '레드브릭', position: 'FrontEnd Engineer', period: ['2022-04-25', '2023-07-24'] }];
+
+  const calcPeriod = (period: string[]) => {
+    const [startDate, endDate] = period;
+    const diff = new Date(endDate).getTime() - new Date(startDate).getTime();
+
+    const diffInMs = Math.abs(diff);
+
+    const days = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
+    const years = Math.floor(days / 365);
+    const months = Math.floor((days % 365) / 30);
+    return `${years}년 ${months}개월`;
+  };
+
   return (
-    <div className='bg-sub-yellow flex flex-col justify-center items-center px-12 py-12 pb-20 gap-12 max-w-6xl m-auto rounded-bl-[7rem] rounded-br-[7rem]	'>
+    <div className='bg-white flex flex-col justify-center items-center px-12 py-12 pb-20 gap-12 max-w-6xl m-auto rounded-bl-[7rem] rounded-br-[7rem]	'>
       <IntroduceBox />
+      <Section title='Work Experience'>
+        <div className='flex flex-col gap-6'>
+          {WORKS.map((work) => (
+            <div key={work.name}>
+              <p className='text-3xl font-semibold'>{work.name}</p>
+              <p className='text-base font-light'>{work.position}</p>
+              <p className='font-light text-sm'>{`${format(new Date(work.period[0]), 'YYYY.MM')} ~ ${format(
+                new Date(work.period[1]),
+                'YYYY.MM'
+              )} (${calcPeriod(work.period)})`}</p>
+            </div>
+          ))}
+        </div>
+      </Section>
       <Section title='Projects'>
         <Project title='morak' description='hihi' githubUrl='' url='' stacks={['d', 'fd']} details={['ddd', 'ddfd']} />
       </Section>
--- components/landing/Section.tsx
+++ components/landing/Section.tsx
@@ -5,10 +5,8 @@ type SectionProps = {
 export default function Section({ title, children }: SectionProps) {
   return (
     <div className='w-full'>
-      <div className='grid grid-cols-1 divide-y divide-main-blue'>
-        <h2 className='text-start my-4 text-lg text-main-blue font-bold'>{title}</h2>
-        <div className=''>{children}</div>
-      </div>
+      <h2 className='text-start my-4 text-4xl text-main-blue font-bold'>{title}</h2>
+      <div className=''>{children}</div>
     </div>
   );
 }
