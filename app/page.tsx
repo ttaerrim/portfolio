fdc90c53c850a63e162eed90f47f218a4130dcc7
ttaerrim <ltr0121@naver.com>
2024-03-04 01:34:02 +0900

    ♻️ 메인 페이지 컴포넌트 분리

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,5 @@
-'use client';
-import Landing from '@/components/landing/main';
-import Footer from '@/components/landing/Footer';
-import Introduce from '@/components/landing/Introduce';
-import { useEffect, useRef, useState } from 'react';
-import Header from '@/components/ui/Header';
-import Light from '@/components/light';
-import Cover from '@/components/cover';
+import Main from '@/components/pages/main';
 
 export default function Home() {
-  const portfolioRef = useRef<HTMLDivElement>(null);
-  const onClickPortfolio = () => {
-    portfolioRef.current?.scrollIntoView({ behavior: 'smooth', inline: 'nearest' });
-  };
-
-  const [isFinishedIntro, setIsFinishedIntro] = useState(false);
-  const [isShowBackground, setIsShowBackground] = useState(false);
-
-  const handleBackground = (isShow: boolean) => setIsShowBackground(isShow);
-
-  const handleIntro = (isFinished: boolean) => setIsFinishedIntro(isFinished);
-
-  return (
-    <>
-      {!isFinishedIntro &&
-        (isShowBackground ? <Cover handleIntro={handleIntro} /> : <Light handleBackground={handleBackground} />)}
-      <Header />
-      <Landing onClickPortfolio={onClickPortfolio} />
-      <Introduce portfolioRef={portfolioRef} />
-      <Footer />
-    </>
-  );
+  return <Main />;
 }
---
+++ components/pages/main/index.tsx
@@ -0,0 +1,33 @@
+'use client';
+import { useRef, useState } from 'react';
+import Landing from '@/components/landing/main';
+import Footer from '@/components/landing/Footer';
+import Introduce from '@/components/landing/Introduce';
+import Header from '@/components/ui/Header';
+import Light from '@/components/light';
+import Cover from '@/components/cover';
+
+export default function Main() {
+  const portfolioRef = useRef<HTMLDivElement>(null);
+  const onClickPortfolio = () => {
+    portfolioRef.current?.scrollIntoView({ behavior: 'smooth', inline: 'nearest' });
+  };
+
+  const [isFinishedIntro, setIsFinishedIntro] = useState(false);
+  const [isShowBackground, setIsShowBackground] = useState(false);
+
+  const handleBackground = (isShow: boolean) => setIsShowBackground(isShow);
+
+  const handleIntro = (isFinished: boolean) => setIsFinishedIntro(isFinished);
+
+  return (
+    <>
+      {!isFinishedIntro &&
+        (isShowBackground ? <Cover handleIntro={handleIntro} /> : <Light handleBackground={handleBackground} />)}
+      <Header />
+      <Landing onClickPortfolio={onClickPortfolio} />
+      <Introduce portfolioRef={portfolioRef} />
+      <Footer />
+    </>
+  );
+}
